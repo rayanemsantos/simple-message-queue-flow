c7a82c92cc5c3ae53b4618c06e04da6cc1cdce8c
Rayane Santos <rayanesantos-131@hotmail.com>
2022-04-11 21:43:18 -0300

    ajuste envio de email

--- order/utils.py
+++ order/utils.py
@@ -6,10 +6,10 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = 'rayanesantostestes@gmail.com'
+app.config['MAIL_PASSWORD'] = 'etipuscscclhvoim'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
@@ -19,10 +19,11 @@ class EmailSender:
     def send(self, data):
         date = datetime.datetime.now() + datetime.timedelta(days=10)
         date = date.strftime('%d/%m/%Y')
-        body = "O pedido # {} com o total de R$ {} foi cadastrado com sucesso. O prazo de entrega será até {}.".format(data['code'], data['value'], date)    
+        body = "O pedido #{} com o total de R${} foi cadastrado com sucesso. O prazo de entrega será até {}.".format(data['code'], data['value'], date)    
         msg = Message("Novo pedido!", sender='no-reply@gmail.com', recipients=[data['email']])
         msg.body = body
-        mail.send(msg)       
+        with app.app_context():
+            mail.send(msg)       
 
 class Listener(stomp.ConnectionListener):
 
